b39c67d1846403d78ded3b72b957ee85e02b455e
Renato Ribeiro <renato.i.a.ribeiro@gmail.com>
2025-08-26 14:32:04 +0100

    Bump swift-syntax to 510.0.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ var package = Package(
         // Depend on the Swift 5.9 release of SwiftSyntax
         .package(
             url: "https://github.com/apple/swift-syntax.git",
-            from: "509.0.0"
+            from: "510.0.0"
         )
     ]
 )
